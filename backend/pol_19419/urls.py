6289d9bc8d3c5228dc37caeb1e7ed2c20a2918d1
Crowdbotics <team@crowdbotics.com>
2020-08-06 10:10:27 +0000

    Update models

--- backend/pol_19419/urls.py
+++ backend/pol_19419/urls.py
@@ -30,6 +30,9 @@ urlpatterns = [
     # Override email confirm to use allauth's HTML view instead of rest_auth's API view
     path("rest-auth/registration/account-confirm-email/<str:key>/", confirm_email),
     path("rest-auth/registration/", include("rest_auth.registration.urls")),
+    path("api/v1/", include("event.api.v1.urls")),
+    path("event/", include("event.urls")),
+    path("home/", include("home.urls")),
 ]
 
 admin.site.site_header = "pol"
@@ -38,15 +41,11 @@ admin.site.index_title = "pol Admin"
 
 # swagger
 api_info = openapi.Info(
-    title="pol API",
-    default_version="v1",
-    description="API documentation for pol App",
+    title="pol API", default_version="v1", description="API documentation for pol App",
 )
 
 schema_view = get_schema_view(
-    api_info,
-    public=True,
-    permission_classes=(permissions.IsAuthenticated,),
+    api_info, public=True, permission_classes=(permissions.IsAuthenticated,),
 )
 
 urlpatterns += [
